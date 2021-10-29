35dae44e9f6db0b42189070d3311eb803c7fbd02
Amine Abdelli <amine.abdelli@outlook.fr>
2021-10-29 16:02:31 +0200

    feat(test): Create a dummy query resolver

--- src/resolvers/hello/hello.ts
+++ src/resolvers/hello/hello.ts
@@ -1,9 +1,11 @@
 async function hello() {
   try {
-    console.log("This resolver is about to send a message")
+    console.log('This resolver is about to send a message');
     // Here some prisma query to database to do some randome stuff
-    console.log("Task done !")
-    return "This is some random data returned"
+    console.log('Task done !');
+    return {
+      message: 'This is some random data returned'
+    };
   } catch (e) {
     console.log();
     (e);
