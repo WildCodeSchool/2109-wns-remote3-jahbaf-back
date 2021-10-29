5c37656215c8432dde98bb1558f38457db16def3
Amine Abdelli <amine.abdelli@outlook.fr>
2021-10-29 15:59:34 +0200

    feat(user): Create function creating one user

--- src/repositories/AuthenticationRepository/createOneUser.ts
+++ src/repositories/AuthenticationRepository/createOneUser.ts
@@ -1,1 +1,19 @@
-// Prisma queries
+import { prisma } from 'src/client';
+import { ICreateUserArgs } from 'src/interfaces';
+
+async function createOneUser({email, password, name}: ICreateUserArgs) {
+  console.log('TRYING TO CREATE A USER');
+  return await prisma.user.create({
+    data: {
+      email,
+      password,
+      name
+    },
+    select: {
+      id: true,
+      email: true,
+    },
+  });
+}
+
+export { createOneUser };
