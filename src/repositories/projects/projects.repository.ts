b7973e524145c44c99cc8da4913f36028a87f961
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-11-14 16:46:59 +0100

    :sparkles: Write repository layer to interact through prisma with database (creation/insertion new project feature)

---
+++ src/repositories/projects/projects.repository.ts
@@ -0,0 +1,19 @@
+import { prisma } from 'src/client';
+import { Project } from 'src/interfaces';
+import { ProjectInput } from 'src/types';
+
+const createOneProject = async (projectInput: ProjectInput): Promise<Project> => {
+    const { name } = projectInput;
+    console.log('CREATING A NEW PROJECT THROUGH PRISMA');
+    return await prisma.project.create({
+        data: {
+            name
+        },
+        select: {
+            id: true,
+            name: true
+        }
+    });
+};
+
+export { createOneProject };
