c60a3e9456f48a70842e0e0b7e1ac87473855b86
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-11-23 20:02:10 +0100

    feat(project): BE-002 set description as optional when creating a project

--- src/services/projects/projects.service.ts
+++ src/services/projects/projects.service.ts
@@ -9,13 +9,11 @@ import { ProjectInput } from 'src/types';
 export const createProjectService = async (
     projectInput: ProjectInput
 ): Promise<Project> => {
-    const { name, description } = projectInput;
+    const { name } = projectInput;
     if (!name) {
         throw new MissingMandatoryFieldException();
     }
-    if (!description) {
-        throw new MissingMandatoryFieldException();
-    }
+
     if (await findProjectByName(name)) {
         throw new DuplicateProjectException();
     }
