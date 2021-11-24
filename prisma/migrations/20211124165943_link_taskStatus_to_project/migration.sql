2e6e126c9ab892b036384a24204b2b1558686b1e
BrianBTI <bertili.brian@gmail.com>
2021-11-24 18:05:09 +0100

    fix(prisma) BE-006 link task-status to a project

---
+++ prisma/migrations/20211124165943_link_taskStatus_to_project/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Status` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Task" DROP CONSTRAINT "Task_status_id_fkey";
+
+-- DropTable
+DROP TABLE "Status";
+
+-- CreateTable
+CREATE TABLE "TaskStatus" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "project_id" TEXT NOT NULL,
+
+    CONSTRAINT "TaskStatus_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "TaskStatus" ADD CONSTRAINT "TaskStatus_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_status_id_fkey" FOREIGN KEY ("status_id") REFERENCES "TaskStatus"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
