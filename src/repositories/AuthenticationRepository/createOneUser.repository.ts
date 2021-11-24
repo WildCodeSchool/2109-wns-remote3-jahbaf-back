8a57e816e9e21f6928086caf4540ac3de29bfb54
Amine Abdelli <amine.abdelli@outlook.fr>
2021-11-24 10:27:41 +0100

    feat(createOneUser): Suffix repositories with .repository.ts

---
+++ src/repositories/AuthenticationRepository/createOneUser.repository.ts
@@ -0,0 +1,16 @@
+import { prisma } from 'src/client';
+import { ICreateUserArgs } from 'src/interfaces';
+
+export const createOneUser = async ({ email, password, name }: ICreateUserArgs) => {
+    return await prisma.user.create({
+        data: {
+            email,
+            password,
+            name
+        },
+        select: {
+            id: true,
+            email: true,
+        },
+    });
+};
