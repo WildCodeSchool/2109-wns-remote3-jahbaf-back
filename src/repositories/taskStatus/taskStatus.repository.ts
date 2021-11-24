1b176ecca3dc7a52d7a9144d13932c2450697bd9
BrianBTI <bertili.brian@gmail.com>
2021-11-24 17:50:24 +0100

    fix(task-status): BE-017 add async / await

--- src/repositories/taskStatus/taskStatus.repository.ts
+++ src/repositories/taskStatus/taskStatus.repository.ts
@@ -1,9 +1,10 @@
 import { prisma } from 'src/client';
+import { TaskStatus } from 'src/interfaces';
 import { TaskStatusInput } from 'src/types';
 
-export const createOneTaskStatus = (taskStatusInput: TaskStatusInput) => {
+export const createOneTaskStatus = async (taskStatusInput: TaskStatusInput): Promise<TaskStatus> => {
     const { name } = taskStatusInput;
-    prisma.taskStatus.create({
+    return await prisma.taskStatus.create({
         data: {
             name,
         },
