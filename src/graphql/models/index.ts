dff7e57ef073995efab027784c6a0ca6478596da
Amine Abdelli <amine.abdelli@outlook.fr>
2021-11-25 12:16:01 +0100

    fix: Delete hello resolver that is now useless

--- src/graphql/models/index.ts
+++ src/graphql/models/index.ts
@@ -1,10 +1,9 @@
 import { AuthTypes } from './auth/Types';
-import { RandomTypes } from './hello/Types';
 import { Mutation } from './Mutations';
 import { ProjectTypes } from './project/Types';
 import { Query } from './Query';
 
-export const Types = [...AuthTypes, ...ProjectTypes, ...RandomTypes];
+export const Types = [...AuthTypes, ...ProjectTypes];
 
 export const typeDefs = [
     Mutation,
--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,3 +1,2 @@
-export * from './hello/hello.query';
 export * from './projects/findManyProjects.query';
 export * from './projects/findOneProject.query';
