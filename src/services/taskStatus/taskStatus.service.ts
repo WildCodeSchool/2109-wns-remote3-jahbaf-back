e50fd035dfe6596bf182993dd7a26b8dac0ef884
BrianBTI <bertili.brian@gmail.com>
2021-11-26 10:14:50 +0100

    feat(task-status): BE-017 review rename variables

--- src/services/taskStatus/taskStatus.service.ts
+++ src/services/taskStatus/taskStatus.service.ts
@@ -1,4 +1,4 @@
-import { AlreadyExistsException } from 'src/exceptions';
+import { TaskAlreadyExistsException } from 'src/exceptions';
 import { TaskStatusInput } from 'src/types';
 import { createOneTaskStatus, findProjectTaskStatusByName } from 'src/repositories/taskStatus/taskStatus.repository';
 import { TaskStatus } from 'src/interfaces';
@@ -6,7 +6,7 @@ import { TaskStatus } from 'src/interfaces';
 export const createTaskStatusService = async (taskStatusInput: TaskStatusInput):Promise<TaskStatus> => {
     const { projectId, name } = taskStatusInput;
     if(projectId && await findProjectTaskStatusByName(projectId, name)) {
-        throw new AlreadyExistsException();
+        throw new TaskAlreadyExistsException();
     }
     return await createOneTaskStatus(taskStatusInput);
 };
