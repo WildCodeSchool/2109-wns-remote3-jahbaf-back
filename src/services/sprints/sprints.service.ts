d41b1b26cc486f08d8b5620135c12e958161d62b
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-11-30 00:02:41 +0100

    feat(sprint): BE-019 :heavy_plus_sign: add check of project existence before creating sprint on it

--- src/services/sprints/sprints.service.ts
+++ src/services/sprints/sprints.service.ts
@@ -1,9 +1,15 @@
 import { Sprint } from '.prisma/client';
+import { ProjectNotFoundException } from 'src/exceptions';
 import { createOneSprint } from 'src/repositories';
 import { SprintInput } from 'src/types';
+import { findProjectByIdService } from '..';
 
 export const createSprintService = async (
     sprintInput: SprintInput
 ): Promise<Sprint> => {
+    const { projectId } = sprintInput;
+    if (!(await findProjectByIdService(projectId))) {
+        throw new ProjectNotFoundException();
+    }
     return await createOneSprint(sprintInput);
 };
