232c1e838d14aa5470c2755323410e9dbe93f509
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-11-29 22:14:36 +0100

    feat(project): BE-020 write updateOneProject repository function + adding project id to UpdateProjectInput transfert type

--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,7 @@ import { Sprint, Project, TaskStatus, Task } from './interfaces';
 export type SprintInput = Omit<Sprint, 'id'>;
 
 export type ProjectInput = Omit<Project, 'id'>;
-export type UpdateProjectInput = Partial<ProjectInput>;
+export type UpdateProjectInput = Pick<Project, 'id'> & Partial<ProjectInput>;
 
 export type TaskStatusInput = Omit<TaskStatus, 'id'>;
 
