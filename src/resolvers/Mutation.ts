7c9ea0125d2c101bcdd9b883a9297dbd12b3a92f
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-11-29 22:50:55 +0100

    feat(project): BE-020 write updateProject resolver

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -2,4 +2,5 @@ export * from './authentication/signup.mutation';
 export * from './sprints/createSprint.mutation';
 export * from './task/createTask.mutation';
 export * from './projects/createProject.mutation';
+export * from './projects/updateProject.mutation';
 export * from './taskStatus/createTaskStatus.mutation';
