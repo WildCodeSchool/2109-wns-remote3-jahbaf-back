4a33b83de2dd5a380e5767c6b1802493546301ac
Amine Abdelli <amine.abdelli@outlook.fr>
2022-02-17 14:34:27 +0100

    feat: Implement documentation

--- src/exceptions/auth/auth.exceptions.ts
+++ src/exceptions/auth/auth.exceptions.ts
@@ -5,7 +5,7 @@ export class InvalidEmailException extends Error {
     message = 'L\'adresse email que vous avez renseigné n\'est pas valide';
 }
 
-export class UserNotFound extends Error {
+export class UserNotFoundException extends Error {
     code = INVALID_EMAIL;
     message = 'Cet utilisateur n\'existe pas';
 }
