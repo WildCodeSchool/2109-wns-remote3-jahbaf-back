080d08f3023063aa889ea0971ea777bf531d31d0
Amine Abdelli <amine.abdelli@outlook.fr>
2022-02-17 14:55:03 +0100

    feat: make name optional

--- src/repositories/AuthenticationRepository/createOneUser.repository.ts
+++ src/repositories/AuthenticationRepository/createOneUser.repository.ts
@@ -1,12 +1,12 @@
 import { prisma } from 'src/client';
 import { ICreateUserArgs } from 'src/interfaces';
 
-export const createOneUser = async ({ email, password, name }: ICreateUserArgs) => {
+export const createOneUser = async ({ email, password, name = '' }: ICreateUserArgs) => {
     return await prisma.user.create({
         data: {
             email,
             password,
-            name
+            name: name,
         },
         select: {
             id: true,
