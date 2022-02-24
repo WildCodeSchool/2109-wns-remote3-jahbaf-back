b95e5562fb1a20c0f4aea170d6ef48d104ed5243
BrianBTI <bertili.brian@gmail.com>
2022-02-24 17:21:45 +0100

    fix task service missing due to conflicts resolve

--- src/services/task/task.service.ts
+++ src/services/task/task.service.ts
@@ -9,9 +9,10 @@ import { TaskInput, UpdateTaskInput } from 'src/types';
 import {
     MissingMandatoryFieldException,
     UnknownTaskStatusException,
+    UserNotFoundException,
 } from 'src/exceptions';
 import { UnknownTaskException } from 'src/exceptions';
-import { findOneProjectTaskStatusById } from 'src/repositories';
+import { findOneProjectTaskStatusById, oneUserById } from 'src/repositories';
 
 export const createTaskService = async (
     taskInput: TaskInput
@@ -21,9 +22,12 @@ export const createTaskService = async (
     return await createOneTask(taskInput);
 };
 
-export const selectOneTaskService = async (id: string): Promise<Task | null> => await findOneTaskById(id);
+export const selectOneTaskService = async (id: string): Promise<Task | null> =>
+    await findOneTaskById(id);
 
-export const selectAllTasksFromProjectService = async (id: string): Promise<Task[] | null> => await findAllTasksFromProject(id);
+export const selectAllTasksFromProjectService = async (
+    id: string
+): Promise<Task[] | null> => await findAllTasksFromProject(id);
 
 export const updateTaskService = async (
     updateInput: UpdateTaskInput
@@ -44,6 +48,23 @@ export const assignTaskStatusService = async (
     const updateInput: UpdateTaskInput = {
         id: taskId,
         statusId: taskStatusId,
+    };
+    return await updateOneTask(updateInput);
+};
+
+export const assignUserService = async (
+    taskId: string,
+    userId: string
+): Promise<Task> => {
+    if (!(await findOneTaskById(taskId))) {
+        throw new UnknownTaskException();
+    }
+    if (!(await oneUserById({ id: userId }))) {
+        throw new UserNotFoundException();
+    }
+    const updateInput: UpdateTaskInput = {
+        id: taskId,
+        userId,
     };
     return await updateOneTask(updateInput);
 };
